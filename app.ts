2f5b59710d73123a0b863a896810c53da0c4c507
Mohamed Abdulla <m3079240@gmail.com>
2025-08-22 15:12:02 +0530

    swaggerunit

--- app.ts
+++ app.ts
@@ -6,14 +6,14 @@ const app = express();
 const swaggerDocument =YAML.load("./docs/yaml")
 
 app.use(express.json());
-
+app.use()
 app.use("/docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument))
 app.use("/api", router);
-
-
+ //
+//port 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-    console.log(`swagger run at http://localhost:3000/docs`);
+    console.log(`swagger run at http://localhost:3000/docss`);
 });
  
